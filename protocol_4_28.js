40e054b6387303924667687e9ff4b9d331b36bfc
kristinezheng <zhengkristine@gmail.com>
2024-05-16 14:28:58 -0400

    may 16

--- protocol_4_28.js
+++ protocol_4_28.js
@@ -27,7 +27,7 @@ function generateProtocol() {
                 {
                     "title": "Welcome!",
                     "listblocks": [{
-                        "text": "Welcome to playing with block towers! In this study, we are looking at when children develop intuitions about the stability of stacked blocks and whether their emotional expressions as a tower is being built correlate with actual changes in the tower’s stability. Please follow the instructions below to participate."
+                        "text": "Welcome to playing with block towers! In this study, we are looking at when children develop intuitions about the stability of stacked blocks and whether their emotional facial expressions correlate with actual changes in the block tower’s stability as the tower is being built. Please follow the instructions below to participate."
                     }]
                 },
                 {
@@ -39,7 +39,7 @@ function generateProtocol() {
                 {
                     "title": "Requirements",
                     "listblocks": [{
-                            "text":  "Please note you will need a laptop or desktop computer (not a mobile device) running Chrome or Firefox to participate."
+                            "text": "Please note you will need a laptop or desktop computer (not a mobile device) running Chrome or Firefox to participate."
                         },
                         {
                             "text": "A quiet and comfortable space for you and your child."
@@ -47,28 +47,31 @@ function generateProtocol() {
                     ]
                 }
             ],
-            "nextButtonText": "Let's Get Started!"
+            "displayFullscreenOverride": true,
+            "nextButtonText": "Let's Get Started!",
         },
         "instructions-2": {
             "kind": "exp-lookit-instructions",
             "blocks": [{
                     "title": "Let's Get Started!",
                     "listblocks": [{
-                        "text": "We will now start the experiment! After you click 'Begin', your computer will take a moment to activate the camera for video recording. You will then see the first block tower movie. The movie will start playing automatically. All your child needs to do is watch the movie. When the movie is over, you can hit the “Next” button to watch the next movie. There are three movies in total."
+                        "text": "We will now start the experiment! After you click 'Begin', your computer will take a moment to activate the camera for video recording. You will then see the first block tower movie. The movie will start playing automatically. All your child needs to do is watch the movie. When the movie is over, you can hit the “NEXT” button to watch the next movie. There are three movies in total."
                     }]
                 },
-                {
-                    "image": {
-                        "src": "https://s3.amazonaws.com/9.85-jenga-towers-2/instructions-image.png",
-                        "alt": "Example of the demo."
-                    }
-                }
+                // {
+                //     "image": {
+                //         "src": "https://s3.amazonaws.com/9.85-jenga-towers-2/instructions-image.png",
+                //         "alt": "Example of the demo."
+                //     }
+                // }
             ],
+            "displayFullscreenOverride": true,
             "nextButtonText": "Begin"
         },
         "video-config": {
             "kind": "exp-video-config",
-            "troubleshootingIntro": "If you're having any trouble getting your webcam set up, please feel free to call Aaron, the graduate student researcher for this project, at (123) 456-7890 and we'd be glad to help you out!"
+            "troubleshootingIntro": "If you're having any trouble getting your webcam set up, please feel email Kristine, a researcher for this project, at kxzheng@mit.edu and we'd be glad to help you out!",
+            "displayFullscreenOverride": true
         },
         "start-recording-with-video": {
             "kind": "exp-lookit-start-recording",
@@ -81,6 +84,44 @@ function generateProtocol() {
             "displayFullscreen": true,
             "waitForVideoMessage": " "
         },
+        "next-button1": {
+            "kind": "exp-lookit-instructions",
+            "blocks": [{
+                "title": "Completed! Click NEXT to proceed to another video."//,
+                // "listblocks": [{
+                //     "text": "Click NEXT to proceed to another video."
+                // }]
+            }],
+            "showPreviousButton": false,
+            "nextButtonText": "NEXT",
+            "displayFullscreenOverride": true,
+        },
+        "next-button2": {
+            "kind": "exp-lookit-instructions",
+            "blocks": [{
+                "title": "Completed! Click NEXT to proceed to another video."//,
+                // "listblocks": [{
+                //     "text": "Click NEXT to proceed to another video."
+                // }]
+            }],
+            "showPreviousButton": false,
+            "nextButtonText": "NEXT",
+            "displayFullscreenOverride": true,
+        },
+        "next-button3": {
+            "kind": "exp-lookit-instructions",
+            "blocks": [{
+                "title": "Completed! Click NEXT to conclude the study."//,
+                // "listblocks": [{
+                //     "text": "Click NEXT to proceed to another video."
+                // }]
+            }],
+            "showPreviousButton": false,
+            "nextButtonText": "NEXT",
+            "displayFullscreenOverride": true,
+        },
+
+
         "stop-recording-with-video": {
             "kind": "exp-lookit-stop-recording",
             "baseDir": "https://www.mit.edu/~kimscott/placeholderstimuli/",
@@ -90,7 +131,7 @@ function generateProtocol() {
             ],
             "video": "attentiongrabber",
             "displayFullscreen": true,
-            "waitForUploadMessage": " "
+            "waitForUploadMessage": "Stopping video recording, please wait!"
         },
         "video-consent": {
             "gdpr": false,
@@ -98,11 +139,12 @@ function generateProtocol() {
             "template": "consent_002",
             "PIName": "Laura Schulz",
             "institution": "Massachusetts Institute of Technology",
-            "purpose": "We are interested in whether both adults and children understand the complex dynamics involved in changing physical events. We are looking at whether we can decode subtle changes in children’s emotional expressions as they watch block towers being built. If so, we might be able to get a continuous measure of children’s changing expectations about the stability of the tower and see whether children’s intuitive physics is sensitive to dynamic changes in events.",
+            "purpose": "We are interested in how children understand complex physical events, like whether a stack of blocks might stay up or fall down. For young children, it can be hard to use complicated language to ask them questions like “On a scale from 1-10, how unstable do you think this tower is?” or “What do you think is the percent chance that this tower will fall if we add one more block to it?” In this study, we want to view subtle changes in children’s emotional expressions as they watch videos of these dynamic events. If we can use their facial expressions to get a continuous measure of children’s changing expectations, then we can use that in future research.",
             "procedures": "First you will review some instructions, and get set up for the study. During the study, your child can sit anywhere where they can comfortably see the screen. They will see three movies of block towers being built. We will ask your child to watch the video and will record your child’s reactions to the videos to see what they attend to and react to. There are no expected risks from taking part in this study.",
-            "payment": "After you finish the study, we will email you an Amazon gift card within three days. We pay $15/hour prorated for the length of the study with a minimum payment of $5/study. We anticipate that this study will take ten minutes to complete so you will receive a $5 gift card. ",
-            "research_rights_statement": "This research has been reviewed and approved by an Institutional Review Board (“IRB”), a group of people who oversee research involving humans as participants. Information to help you understand research is on-line at https://templatestudiesinstitute.edu/irb. You may talk to a IRB staff member at (123) 456-7890 or IRBAdmin@tsi.edu for any of the following: 1) Your questions, concerns, or complaints are not being answered by the research team; 2) you cannot reach the research team; 3) you want to talk to someone besides the research team; 4) you have questions about your rights as a research subject; 5) you want to get information or provide input about this research.",
-            "PIContact": "Laura Schulz (contact: lschulz@mit.edu)"
+            "payment": "After you participate, we will email you a $5 Amazon.com gift card within seven days to thank you for your time (only one per child). To be eligible for compensation, we ask that you (1) provide a valid consent video (we will show you how!), (2) make sure your child is in the age range specified, and (3) ensure that your child is visible during the recorded videos. Your child does not need to finish the entire study in order to be eligible for compensation. There are no other direct benefits to you or your child from participating, but we hope you will enjoy the experience.",
+            "research_rights_statement": "You are not waiving any legal claims, rights, or remedies because of your participation in this research study. If you feel you have been treated unfairly, or you have questions regarding your rights as a research subject, you may contact Laura Schulz at lschulz@mit.edu.",
+            "PIContact": "Laura Schulz (contact: lschulz@mit.edu)",
+            "displayFullscreenOverride": true,
         },
         "positioning-config": {
             "kind": "exp-video-config-quality",
@@ -124,22 +166,24 @@ function generateProtocol() {
                     "text": "Take a few moments to get settled and make sure your child's face is clearly visible in the webcam preview to the right. You may need to adjust the webcam angle or turn on a light to make sure his or her eyes are visible."
                 },
                 {
-                    "text": "The green button down below will start the study. Press the button once you and your child are ready to go!",
-                    "title": "When you're ready, press the green button and close your eyes"
+                    "text": "The green Next button will start the study. Press the button once you and your child are ready to go!",
+                    "title": "When you're ready, press the green button."
                 }
             ],
             "requireItemConfirmation": true,
-            "recordingInstructionText": ""
+            "recordingInstructionText": "",
+            "displayFullscreenOverride": true
         },
         "my-exit-survey": {
             "kind": "exp-lookit-exit-survey",
             "debriefing": {
-                "text": "<b>Purpose</b> <br> Thank you for participating in our experiment! This might seem like a very simple task: just watching movies of block towers being built. However, your child is helping us learn something important: whether children’s emotional expressions correlate with changes in the stability of these block towers. <br><br><b>Experimental Design</b> <br> We showed your child three short movies of blocks being stacked one by one. Your child’s facial expressions and body language was recorded as they watched the movies. We expect that children may react with a subtle change in their emotional expressions to certain moments in the videos when the block tower becomes unstable. However, we might be wrong! Your child may have no particular reaction to our movies or no reaction that correlates with our predictions. In our studies, the children are always correct; their responses tell us if, when, and how our methods and ideas need to change. <br><br><b>Why this matters</b><br>The physical properties of events involve many complex dynamics. We are interested in children’s sensitivity to these complex changes. We can’t easily ask young children to give us a rating of a tower’s stability on a 0-100 scale at each time point as the tower is being built but we think we might be able to use children’s natural reactions (e.g., increasing suspense or anticipation) to get a continuous measure of their changing expectations about the towers’ stability. We can then see if children’s responses correlate with models of the actual physics and changing stability of these towers – and if children’s expectations about these events change with age and experience. This study might give us insight into the development of our intuitions about  objects and forces and help us understand more about how children learn about the physical world. Thank you so much for participating in our study! Research on child development would not be possible without your support! <br><br>",
+                "text": "<b>Purpose</b> <br> Thank you for participating in our experiment! This might seem like a very simple task: just watching movies of block towers being built. However, your child is helping us learn something important: whether children’s emotional expressions correlate with changes in the stability of these block towers. <br><br><b>Payment</b><br> Once we confirm your consent video, we will email you a $5 Amazon.com gift card within seven days to thank you for your time (only one per child). To be eligible for compensation, you (1) provided a valid consent video, (2) made sure your child is in the age range specified, and (3) ensured that your child was visible during the recorded videos. Your child did not need to finish the entire study in order to be eligible for compensation. There are no other direct benefits to you or your child from participating, but we hope you enjoyed the experience. <br><br><b> Experimental Design</b> <br> We showed your child three short movies of blocks being stacked one by one. Your child’s facial expressions and body language were recorded as they watched the movies. We expect that children may react with a subtle change in their emotional expressions to certain moments in the videos when the block tower becomes unstable. However, we might be wrong! Your child may have had no particular reaction to our movies or no reaction that correlates with our predictions. In our studies, the children are always correct; their responses tell us if, when, and how our methods and ideas need to change. <br><br><b>Why this matters</b><br>The physical properties of events involve many complex dynamics. We are interested in children’s sensitivity to these complex changes. We can’t easily ask young children to give us a rating of a tower’s stability on a 0-10 scale at each time point as the tower is being built but we think we might be able to use children’s natural reactions (e.g., increasing suspense or anticipation) to get a continuous measure of their changing expectations about the towers’ stability. We can then see if children’s responses correlate with models of the actual physics and changing stability of these towers – and if children’s expectations about these events change with age and experience. This study might give us insight into the development of our intuitions about objects and forces and help us understand more about how children learn about the physical world. Thank you so much for participating in our study! Research on child development would not be possible without your support! <br><br><b>Learn More </b><br> Check out the broader developmental research that our experiment builds upon here: <a href='https://news.mit.edu/2011/infant-cognition-0527' target='_blank' rel='noopener'> Click to learn about “Inside the infant mind.“</a> Feel free to contact Laura Schulz at lschulz@mit.edu if you have further questions! ",
                 "image": {
                     "src": "https://myblocks.s3.us-east-2.amazonaws.com/example_image.png",
                     "alt": "video screenshots"
                 },
-                "title": "Thank you for participating in our study!"
+                "title": "Thank you for participating in our study!",
+                "displayFullscreenOverride": true
             }
         },
         "study-completion": {
@@ -165,6 +209,7 @@ function generateProtocol() {
             "pauseAudio": "pause",
             "pauseVideo": "Elmo",
             "pauseText": "",
+            "displayFullscreenOverride": true,
             "unpauseAudio": "return_after_pause"
         }
 
@@ -293,9 +338,9 @@ function generateProtocol() {
         //"showProgressBar": true,
         "backgroundColor": "white",
         "doRecording": true,
-        "autoProceed": false,
+        "autoProceed": true,
         "restartAfterPause": false,
-        "nextButtonEnabled": true,
+        "nextButtonEnabled": false,
 
         "basedir": "https://myblocks.s3.us-east-2.amazonaws.com/",
         "videoTypes": [
@@ -324,9 +369,9 @@ function generateProtocol() {
         },
         "backgroundColor": "white",
         "doRecording": true,
-        "autoProceed": false,
+        "autoProceed": true,
         "restartAfterPause": false,
-        "nextButtonEnabled": true,
+        "nextButtonEnabled": false,
 
         "basedir": "https://myblocks.s3.us-east-2.amazonaws.com/",
         "videoTypes": [
@@ -356,9 +401,9 @@ function generateProtocol() {
         //"showProgressBar": true,
         "backgroundColor": "white",
         "doRecording": true,
-        "autoProceed": false,
+        "autoProceed": true,
         "restartAfterPause": false,
-        "nextButtonEnabled": true,
+        "nextButtonEnabled": false,
 
         "basedir": "https://myblocks.s3.us-east-2.amazonaws.com/",
         "videoTypes": [
@@ -405,8 +450,11 @@ function generateProtocol() {
         "instructions-2",
         "start-recording-with-video",
         "block-trials1",
+        "next-button1",
         "block-trials2",
+        "next-button2",
         "block-trials3",
+        "next-button3",
         "stop-recording-with-video",
         "study-completion",
         "my-exit-survey"
